60e6388e89ee9ae477169e96d185121342033d2d
Senchibab <senchibab@gmail.com>
2020-09-21 01:24:04 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Ex extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Senchi babu(728741)</h1>
+        <h2>
+          <p>
+            <a href="https://stackblitz.com/edit/react-mhiyup?file=src%2Findex.js">
+              Exercise 1
+            </a>
+          </p>
+        </h2>
+        <h2>
+          <p>
+            <a href="https://stackblitz.com/edit/react-6zawnc?file=src%2Findex.js">
+              Exercise 2
+            </a>
+          </p>
+        </h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Ex />, document.getElementById("root"));
