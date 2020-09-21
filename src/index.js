f3bf7e221ac4d88f489b08ec1ac93f2300fb9ee3
Senchibab <senchibab@gmail.com>
2020-09-21 13:01:52 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -8,41 +8,41 @@ class Ex extends React.Component {
         <h1>Senchi babu(728741)</h1>
         <h2>
           <p>
-            <a href="https://stackblitz.com/edit/react-xeqbvy">Exercise 1</a>
+            <a href="https://stackblitz.com/edit/react-xeqbvy" target="_blank">Exercise 1</a>
           </p>
         </h2>
         <h2>
           <p>
-            <a href="https://stackblitz.com/edit/react-ry4w5c">Exercise 2</a>
+            <a href="https://stackblitz.com/edit/react-ry4w5c" target="_blank" >Exercise 2</a>
           </p>
         </h2>
         <h2>
           <p>
-            <a href="https://stackblitz.com/edit/react-xb9zft">Exercise 3</a>
+            <a href="https://stackblitz.com/edit/react-xb9zft" target="_blank">Exercise 3</a>
           </p>
         </h2>
         <h2>
           <p>
-            <a href="https://stackblitz.com/edit/react-7antli">Exercise 4</a>
+            <a href="https://stackblitz.com/edit/react-7antli" target="_blank" >Exercise 4</a>
           </p>
         </h2>
         <h2>
           <p>
-            <a href="https://stackblitz.com/edit/react-u381hz">
+            <a href="https://stackblitz.com/edit/react-u381hz" target="_blank">
               Exercise 5 with arrow function
             </a>
           </p>
         </h2>
         <h2>
           <p>
-            <a href="https://stackblitz.com/edit/react-zu7yqf">
+            <a href="https://stackblitz.com/edit/react-zu7yqf" target="_blank">
               Exercise 5 without arrow function
             </a>
           </p>
         </h2>
         <h2>
           <p>
-            <a href="https://stackblitz.com/edit/react-blfyge">Exercise 6</a>
+            <a href="https://stackblitz.com/edit/react-blfyge" target="_blank">Exercise 6</a>
           </p>
         </h2>
       </div>
